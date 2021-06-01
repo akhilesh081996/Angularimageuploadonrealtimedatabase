2ae873f6a511dec9b792d5d2b3771523a124ec49
unknown <akhilesh.sharma.dev@gmail.com>
2021-06-01 15:05:20 +0530

    'first'

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+    firebase : {
+    apiKey: "AIzaSyA4qn-_Te7dS3L4LftG2k8t6FK9JvnRtkg",
+    authDomain: "demofirebaseimageupload.firebaseapp.com",
+    projectId: "demofirebaseimageupload",
+    databaseURL: 'https://demofirebaseimageupload-default-rtdb.firebaseio.com',
+    storageBucket: "demofirebaseimageupload.appspot.com",
+    messagingSenderId: "472434592597",
+    appId: "1:472434592597:web:747d6982be1c4cfdd23ddc",
+   
+  }
+};
