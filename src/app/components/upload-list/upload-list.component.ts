6ee25ccd13d608b9c52640a1a0d596ad53718caf
unknown <akhilesh.sharma.dev@gmail.com>
2021-06-01 16:26:03 +0530

    'video_image_both'

--- src/app/components/upload-list/upload-list.component.ts
+++ src/app/components/upload-list/upload-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FileUploadService } from 'src/app/services/file-upload.service';
 import { map } from 'rxjs/operators';
+import { newArray } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-upload-list',
@@ -11,6 +12,12 @@ export class UploadListComponent implements OnInit {
   fileUploads: any[];
   fileUploadsimages;
 
+  // imagearray :any []
+  // videoarray : any []
+  newarray :any []
+  videoarray: number[] = [];
+  imagearray: number[] = [];
+
   constructor(private uploadService: FileUploadService) { }
 
   ngOnInit(): void {
@@ -20,9 +27,22 @@ export class UploadListComponent implements OnInit {
         changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
       )
     ).subscribe(fileUploads => {
+      this.videoarray= [];
+      this.imagearray= [];
       this.fileUploads = fileUploads;
-      this.fileUploadsimages = fileUploads[0].url;
-      console.log(this.fileUploadsimages,'this.fileUploads')
+      console.log(this.fileUploads,'this.fileUploads')
+      // this.fileUploadsimages = fileUploads[0].url;
+for (let i = 0; i < this.fileUploads.length; i++) {
+  const name =  fileUploads[i].name
+ if( fileUploads[i].name.match(/mp4/g)) {
+  this.videoarray.push( this.fileUploads[i])
+  // console.log(this.videoarray,'this.videoarray')
+ }
+ else{
+  this.imagearray.push( this.fileUploads[i])
+  // console.log(this.imagearray,'this.imagearray')
+ }   
+}
     });
   }
 }
